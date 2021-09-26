615e4f6011147311ead1d543b7cbbb5a655bc848
naveen962 <mrn28897@gmail.com>
2021-09-25 20:14:47 -0700

    monthly payment program is added

---
+++ MonthlyPayment/MonthlyPayment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonthlyPayment
+    {
+        class Program
+        {
+        public static void MontlyPayment(int Principleamount, int NoOfYear, float Rate)
+        {
+            float r = Rate / (12 * 100);
+            int n = 12 * NoOfYear;
+            double payment = (Principleamount * r) / (1 - Math.Pow((1 + r), -n));
+            Console.WriteLine("Montly payment will be {0}", payment);
+        }
+            static void Main(string[] args)
+            {
+                
+                Console.WriteLine("Enter Principle amount, no. of years and Rate of intrest");
+                int Principleamount = int.Parse(Console.ReadLine());
+                int NoOfYear = int.Parse(Console.ReadLine());
+                int Rate = int.Parse(Console.ReadLine());
+                Program.MontlyPayment(Principleamount, NoOfYear, Rate);
+        }
+    }
+}
